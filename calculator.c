62e70154563db22728b2484d6da17374c87bef27
정지훈 <121081091+JJHgit2@users.noreply.github.com>
2022-12-21 14:38:39 +0900

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "eval.h"
+#include "eval.h"https://github.com/JJHgit2/calculator/blob/main/calculator.c
 #define BUFFERSIZE (256)
 
 int main(void) {
@@ -10,5 +10,6 @@ int main(void) {
 	fflush(stdout);
 	fgets(input, BUFFERSIZE, stdin);
 	printf("Result is: %.2f\n", evaluate(input));
+	printf("201730302 END\n");
 	return EXIT_SUCCESS;
 }
